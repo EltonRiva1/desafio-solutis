010def92460a0cf9e2e022172a80f31ba8dccf25
notle <notle@DESKTOP-FA34NT3>
2018-09-10 23:22:40 -0300

    Commit inicial

---
+++ src/main/java/br/com/solutis/desafiosolutis/model/Vogal.java
@@ -0,0 +1,34 @@
+package br.com.solutis.desafiosolutis.model;
+
+public class Vogal {
+	private String entradaDados, vogal, tempoTotal;
+
+	public Vogal() {
+		super();
+	}
+
+	public String getEntradaDados() {
+		return entradaDados;
+	}
+
+	public void setEntradaDados(String entradaDados) {
+		this.entradaDados = entradaDados;
+	}
+
+	public String getVogal() {
+		return vogal;
+	}
+
+	public void setVogal(String vogal) {
+		this.vogal = vogal;
+	}
+
+	public String getTempoTotal() {
+		return tempoTotal;
+	}
+
+	public void setTempoTotal(String tempoTotal) {
+		this.tempoTotal = tempoTotal;
+	}
+
+}
